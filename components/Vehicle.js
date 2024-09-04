20cf3be2ac8f9f99259887a7b06b203b400f775f
khushi srivastava <srivastavakhushi1601@gmail.com>
2024-09-04 20:21:23 +0530

    updates

--- components/Vehicle.js
+++ components/Vehicle.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/display-name */
+
 import React, { useRef, useImperativeHandle, forwardRef, useEffect } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import { RigidBody, useRapier } from '@react-three/rapier'
@@ -86,5 +88,5 @@ const Vehicle = forwardRef(({ onCollision }, ref) => {
     </RigidBody>
   )
 })
-
+Vehicle.displayName = 'Vehicle'
 export default Vehicle
